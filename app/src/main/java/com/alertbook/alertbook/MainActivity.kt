100b393a425f372b9c0e6e89aee0f9c411065cce
surajmaity1 <surajmaity981@gmail.com>
2023-02-27 09:49:23 +0530

    Splash Screen API added

--- app/src/main/java/com/alertbook/alertbook/MainActivity.kt
+++ app/src/main/java/com/alertbook/alertbook/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
@@ -20,73 +21,35 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.alertbook.alertbook.models.SplashViewModel
 import com.alertbook.alertbook.ui.theme.AlertBookTheme
 import kotlinx.coroutines.delay
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: SplashViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
+
+        splashScreen.setKeepOnScreenCondition{viewModel.isLoading.value}
+
         setContent {
             AlertBookTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Navigation()
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ){
+                        Text(text = "Main Activity")
+                    }
                 }
             }
         }
     }
-}
-
-@Composable
-fun Navigation() {
-    val navCtl = rememberNavController()
-    NavHost(navController = navCtl, startDestination = "splash_screen"){
-        composable("splash_screen"){
-            SplashScreen(navController = navCtl)
-        }
-        composable("main_screen"){
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
-            ){
-                Text(text = "MAIN SCREEN", color = Color.Blue)
-            }
-        }
-    }
-}
-
-@Composable
-fun SplashScreen(navController: NavController) {
-    val scaleUp = remember {
-        Animatable(0f)
-    }
-    LaunchedEffect(key1 = true){
-        scaleUp.animateTo(
-            targetValue = 2.0f,
-            animationSpec = tween(
-                durationMillis = 1000,
-                easing = {
-                    OvershootInterpolator(3f).getInterpolation(it)
-                }
-            )
-        )
-        delay(1000L)
-        navController.navigate("main_screen") {
-            popUpTo(id = 0)
-        }
-    }
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.ic_launcher_foreground),
-            contentDescription = "logo",
-            modifier = Modifier.scale(scaleUp.value)
-        )
-    }
-
 }
